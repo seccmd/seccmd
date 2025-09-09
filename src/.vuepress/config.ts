92ea89dda367339905389583344395e71b4b101d
SecCMD <79789833+seccmd@users.noreply.github.com>
2025-09-09 10:06:26 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,8 +6,8 @@ export default defineUserConfig({
   base: "/pydeep/",
 
   lang: "zh-CN",
-  title: "文档演示",
-  description: "vuepress-theme-hope 的文档演示",
+  title: "土豆哥 Python 进阶之路",
+  description: "这是一份通俗易懂、轻松愉快的Python学习指南，内容涵盖Python基础、Python并发编程、Python企业级开发、Python面试等核心知识点。",
 
   theme,
 
